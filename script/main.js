c40909b48f28015ff7f4108ef77bd89e1900f630
Sharon-uzu <109592507+Sharon-uzu@users.noreply.github.com>
2022-11-10 04:17:04 -0800

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,135 @@
+var open = document.getElementById('open');
+var link = document.getElementById('link');
+var close = document.getElementById('close');
+
+open.addEventListener('click', function(){
+    link.style.display = 'block';
+    close.style.display = 'block';
+    open.style.display = 'none'
+})
+
+close.addEventListener('click', function(){
+    link.style.display = 'none';
+    close.style.display = 'none';
+    open.style.display = 'block'
+})
+
+
+// ask
+var p1 = document.getElementById('p1');
+var imga1 = document.getElementById('imga1');
+var imgb1 = document.getElementById('imgb1');
+
+imga1.addEventListener('click', function() {
+    p1.style.display = 'none';
+    imga1.style.display = 'none';
+    imgb1.style.display = 'block'
+
+})
+
+imgb1.addEventListener('click', function() {
+    p1.style.display = 'block';
+    imgb1.style.display = 'none';
+    imga1.style.display = 'block'
+
+})
+
+
+
+var p2 = document.getElementById('p2');
+var imga2 = document.getElementById('imga2');
+var imgb2 = document.getElementById('imgb2');
+
+imgb2.addEventListener('click', function() {
+    p2.style.display = 'block';
+    imgb2.style.display = 'none';
+    imga2.style.display = 'block'
+
+})
+
+imga2.addEventListener('click', function() {
+    p2.style.display = 'none';
+    imga2.style.display = 'none';
+    imgb2.style.display = 'block'
+
+})
+
+
+
+var p3 = document.getElementById('p3');
+var imga3 = document.getElementById('imga3');
+var imgb3 = document.getElementById('imgb3');
+
+    imgb3.addEventListener('click', function() {
+    p3.style.display = 'block';
+    imgb3.style.display = 'none';
+    imga3.style.display = 'block'
+
+})
+
+imga3.addEventListener('click', function() {
+    p3.style.display = 'none';
+    imga3.style.display = 'none';
+    imgb3.style.display = 'block'
+
+})
+
+
+var p4 = document.getElementById('p4');
+var imga4 = document.getElementById('imga4');
+var imgb4 = document.getElementById('imgb4');
+
+    imgb4.addEventListener('click', function() {
+    p4.style.display = 'block';
+    imgb4.style.display = 'none';
+    imga4.style.display = 'block'
+
+})
+
+imga4.addEventListener('click', function() {
+    p4.style.display = 'none';
+    imga4.style.display = 'none';
+    imgb4.style.display = 'block'
+
+})
+
+
+var p5 = document.getElementById('p5');
+var imga5 = document.getElementById('imga5');
+var imgb5 = document.getElementById('imgb5');
+
+imgb5.addEventListener('click', function() {
+    p5.style.display = 'block';
+    imgb5.style.display = 'none';
+    imga5.style.display = 'block'
+
+})
+
+imga5.addEventListener('click', function() {
+    p5.style.display = 'none';
+    imga5.style.display = 'none';
+    imgb5.style.display = 'block'
+
+})
+
+
+var p6 = document.getElementById('p6');
+var imga6 = document.getElementById('imga6');
+var imgb6 = document.getElementById('imgb6');
+
+imgb6.addEventListener('click', function() {
+    p6.style.display = 'block';
+    imgb6.style.display = 'none';
+    imga6.style.display = 'block'
+
+})
+
+imga6.addEventListener('click', function() {
+    p6.style.display = 'none';
+    imga6.style.display = 'none';
+    imgb6.style.display = 'block'
+
+})
+
+
+
